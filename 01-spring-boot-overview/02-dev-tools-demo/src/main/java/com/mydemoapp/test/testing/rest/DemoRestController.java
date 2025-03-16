07c4bc055c4906f80f1e2e87e004fffb18da4d12
DeepakEuler <deepaksharmaforwork@gmail.com>
2025-03-16 16:58:44 +0530

    correcting project structure and adding for dev tools

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/mydemoapp/test/testing/rest/DemoRestController.java
@@ -0,0 +1,13 @@
+package com.mydemoapp.test.testing.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoRestController {
+    //expose "/" endpoint that will return "Hello World"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}
